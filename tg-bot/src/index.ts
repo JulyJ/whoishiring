aa862b0c85849eae25dd14180b6d8d125bdd138e
Dmitry Malkov <targtn@gmail.com>
2024-06-05 21:13:34 -0600

    Format date

--- tg-bot/src/index.ts
+++ tg-bot/src/index.ts
@@ -4,10 +4,21 @@ import { HnJobMessage } from "./models/hn-job-message";
 
 require("dotenv").config();
 
+function formatDate(dateStr: string): string {
+    const date = new Date(dateStr);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0"); // getMonth() returns 0-indexed month
+    const day = String(date.getDate()).padStart(2, "0");
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+
+    return `${year}/${month}/${day} ${hours}:${minutes}`;
+}
+
 function formatMessage(job: any, originalPost: HnJobMessage) {
     const ogText = originalPost.text.split("\n");
     const title = `<b>${ogText[0]}</b>\n\n`;
-    const date = `<b>Date:</b> ${job.date}\n`;
+    const date = `<b>Date:</b> ${formatDate(job.date)}\n`;
     const company = job.company ? `<b>Company:</b> ${job.company}\n\n` : "";
 
     const urls = job.urls ? `<b>URLs:</b> ${job.urls.join("\n")}\n\n` : "";
